87e7d50cc2ad040fa586efa640cf35cc9ec29f91
Shivaranjith0077 <roaabhi548@gmail.com>
2024-08-29 11:41:38 +0530

    added content to MaxProduct

--- MaximumProduct.js
+++ MaximumProduct.js
@@ -1,3 +1,13 @@
-let b = 10
-let i = "alvitone"
-console.log(i + b)
+let maxProduct=function(nums){
+    let prefix=1,suffix=1,maxi=nums[0]
+    for(let i=0;i<nums.length;i++){
+        if(prefix==0) prefix=nums[i]
+        else prefix*=nums[i]
+        if(suffix==0) suffix=nums[nums.length-1-i]
+        else suffix*=nums[nums.length-1-i]
+        maxi=Math.max(maxi,Math.max(prefix,suffix))
+    }
+    return maxi
+}
+const nums=[-2,0,-1]
+console.log(maxProduct(nums))
