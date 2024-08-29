23700c391879d58dfc68484c90c1ddb3c9f567ca
Shivaranjith0077 <roaabhi548@gmail.com>
2024-08-29 11:18:45 +0530

    added content in MaxSum prob

--- MaximumSum.js
+++ MaximumSum.js
@@ -1,3 +1,16 @@
-let b=10
-let c=20
-console.log(b+c)
+var maxSubArray = function(nums) {
+    if(nums.length<=1){
+        return nums[0]
+    }
+    let sum=0,maxi=nums[0]
+    for(let num of nums){
+        if(sum<0){
+            sum=0
+        }
+        sum+=num
+        maxi=Math.max(maxi,sum)
+    }
+    return maxi
+};
+const nums=[5,4,-1,7,8]
+console.log(maxSubArray(nums))
